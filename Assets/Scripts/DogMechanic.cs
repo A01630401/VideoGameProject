9f5e00ceb4588749dfc5f95b03780345597fb8b0
Fernando <A01630401@itesm.mx>
2019-04-07 20:06:06 -0500

    Dog

---
+++ Assets/Scripts/DogMechanic.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogMechanic : MonoBehaviour
+{
+    private float speed;
+    public Transform owner;
+    private float threshold;
+    private bool followingEnemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 4.0f;
+        threshold = 1.0f;
+        followingEnemy = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.LookAt(owner);
+        if (!followingEnemy) {
+            Vector3 followPointPos = new Vector3(owner.position.x - 3, owner.position.y, owner.transform.position.z);
+            transform.position = Vector3.MoveTowards(transform.position, followPointPos, speed * Time.deltaTime);
+        }
+    }
+
+    void OnTriggerStay(Collider other) {
+        if (other.gameObject.layer == 10) {
+            followingEnemy = true;
+            Transform target = other.transform;
+            transform.LookAt(target.transform);
+
+            Vector3 followPointPos = new Vector3(target.position.x, target.position.y, target.transform.position.z);
+            transform.position = Vector3.MoveTowards(transform.position, followPointPos, speed * Time.deltaTime);
+
+            float d = Vector3.Distance(transform.position, target.position);
+
+            if (d < threshold) {
+                Destroy(other.gameObject);
+                followingEnemy = false;
+            }
+            
+        }
+
+    }
+}
